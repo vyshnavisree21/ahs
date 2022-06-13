edb05304df52716155776a2b9a7d204210050f3c
vyshnavisree21 <vyshnavisree21@gmail.com>
2022-06-13 20:06:32 +0530

    mm

--- frontend/js/app.js
+++ frontend/js/app.js
@@ -8,7 +8,7 @@ window.addEventListener("DOMContentLoaded", async () => {
 
   welcomeH1.innerText = welcome_h1;
   welcomeH2.innerText = welcome_h2;
-  welcomeP.innerHTML = welcome_p;
+
 
   if (window.ethereum) {
     window.web3 = new Web3(window.ethereum);

dc151594eb9522dab9a89db7c7bcaec9977cc877
vyshnavisree21 <vyshnavisree21@gmail.com>
2022-06-18 02:11:48 +0530

    dd

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -3,7 +3,7 @@ const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Alien Community!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The Alien Community ';
+const welcome_p = '';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
